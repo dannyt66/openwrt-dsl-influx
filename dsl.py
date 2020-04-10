2000a44435cb6ac66129766f04e45171053dec51
Daniel T. Thorpe <daniel.thorpe@citrix.com>
2020-04-10 21:59:04 +0100

    feat(dsl.py): pull data and post to influxdb
    hard coded stuff but it works

---
+++ dsl.py
@@ -0,0 +1,48 @@
+import subprocess
+from influxdb import InfluxDBClient
+
+output = subprocess.run(
+    [
+        'ssh',
+        '-i',
+        '/Users/danielthorpe/.ssh/id_rsa',
+        'root@10.187.0.8',
+        '/etc/init.d/dsl_control lucistat'
+    ],
+    capture_output=True
+)
+
+metrics = output.stdout.splitlines()
+
+metricdict = {
+    "measurement": "DSL",
+    "tags": {
+        "modem_identifier": "Cambridge"
+    },
+    "fields": {}
+}
+
+for metric in metrics:
+    try:
+        splitmetric = str(
+            metric
+        ).replace(
+            'b', '', 1
+        ).replace(
+            "'", ""
+        ).split(
+            "="
+        )
+        metricdict["fields"][splitmetric[0]] = splitmetric[1].replace('"', '')
+    except:
+        pass
+
+
+client = InfluxDBClient(
+    host='grafana.ad.dantho.xyz',
+    port=8086,
+    database='modem'
+)
+
+client.write_points([metricdict])
+
